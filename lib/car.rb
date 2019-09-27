1384b8377559332594c8c9f1f5624f32aa123ff4
saramccombs <theSaraMcCombs@gmail.com>
2019-09-27 16:11:07 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,18 @@
+# requiring vehicle.rb because it is the superclass and will need access to the class and thus the file that contains the class
 require_relative "./vehicle.rb"
+
+# We use < to inherit the Car class from Vehicle
+class Car < Vehicle
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+
+end
+
+=begin
+Method Lookup in Ruby: the compiler will first look in the class to which the instance of car that we are calling the method 
+on belongs. If the method exists there it will use it. If no method, then it will move to the parent class. 
+=end
+
